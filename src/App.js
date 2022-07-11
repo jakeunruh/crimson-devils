0bc6e1d3b9de49fbd8589c902a8145ecfe817687
jake unruh <jake.unruh@gmail.com>
2022-07-11 09:05:19 -0500

    upload thumb

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <div className="main__video-wrapper">
           <h2>Watch Crimson Devils at the 2022 Stoner Jam at The Far Out Lounge in Austin, Tx</h2>
           <div className="main__video">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/xl-XjjI-d3Y?start=844" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560"cheight="315" src="https://www.youtube.com/embed/Y8Wp3dafaMQ&autoplay=1" srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/xl-XjjI-d3Y?start=844><img src='' alt='Austin Stoner Jam 2022 - Sampler Vol 1'><span>▶</span></a>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Austin Stoner Jam 2022 - Sampler Vol 1"></iframe>
           </div>
         </div>
         <ul className="main__link-list">
