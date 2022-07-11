8e61c21728009e012dee23d49702ba145d46221e
jake unruh <jake.unruh@gmail.com>
2022-07-10 22:10:32 -0500

    added video

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ function App() {
         <img className="header__img--bird" src={Bird} alt="" />
       </header>
       <main className="main">
+        <div className="main__video-wrapper">
+          <h2>Watch Crimson Devils at the 2022 Stoner Jam at The Far Out Lounge in Austin, Tx</h2>
+          <div className="main__video">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/xl-XjjI-d3Y?start=844" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
         <ul className="main__link-list">
           <li className="main__link-list-item">
             <a className="btn" href="https://crimsondevils.bandcamp.com/" target="_blank" rel="noreferrer">Bandcamp</a>
