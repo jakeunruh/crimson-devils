b89ae12c52638abbd0dc000bc8c9c3fb66fa0d44
jake unruh <jake.unruh@gmail.com>
2022-07-12 13:46:06 -0500

    added soundcloud link

--- src/components/SocialLinkList.jsx
+++ src/components/SocialLinkList.jsx
@@ -10,6 +10,9 @@ function SocialLinkList() {
             <SocialIcon url="https://www.facebook.com/crimsondevils/" bgColor="#b81a0a" color="currentColor" target="_blank" />
         </li>
         <li className="footer__social-link-list-item">
+            <SocialIcon url="https://soundcloud.com/crimsondevilsatx" bgColor="#b81a0a" color="currentColor" target="_blank" />
+        </li>
+        <li className="footer__social-link-list-item">
             <SocialIcon url="https://www.youtube.com/channel/UCCCPUW7c9Ox0CKSKG8iGegg/videos" bgColor="#b81a0a" color="currentColor" target="_blank" />
         </li>
     </ul>
