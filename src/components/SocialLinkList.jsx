9370ac11d0ed31983524f63bd7243e6469463ca2
jake unruh <jake.unruh@gmail.com>
2022-07-12 15:20:58 -0500

    updated red

--- src/components/SocialLinkList.jsx
+++ src/components/SocialLinkList.jsx
@@ -4,16 +4,16 @@ function SocialLinkList() {
   return (
     <ul className="footer__social-link-list">
         <li className="footer__social-link-list-item">
-            <SocialIcon url="https://www.instagram.com/crimsondevilsatx/" bgColor="#b81a0a" color="currentColor" target="_blank" />
+            <SocialIcon url="https://www.instagram.com/crimsondevilsatx/" bgColor="#ac0f1b" color="currentColor" target="_blank" />
         </li>
         <li className="footer__social-link-list-item">
-            <SocialIcon url="https://www.facebook.com/crimsondevils/" bgColor="#b81a0a" color="currentColor" target="_blank" />
+            <SocialIcon url="https://www.facebook.com/crimsondevils/" bgColor="#ac0f1b" color="currentColor" target="_blank" />
         </li>
         <li className="footer__social-link-list-item">
-            <SocialIcon url="https://soundcloud.com/crimsondevilsatx" bgColor="#b81a0a" color="currentColor" target="_blank" />
+            <SocialIcon url="https://soundcloud.com/crimsondevilsatx" bgColor="#ac0f1b" color="currentColor" target="_blank" />
         </li>
         <li className="footer__social-link-list-item">
-            <SocialIcon url="https://www.youtube.com/channel/UCCCPUW7c9Ox0CKSKG8iGegg/videos" bgColor="#b81a0a" color="currentColor" target="_blank" />
+            <SocialIcon url="https://www.youtube.com/channel/UCCCPUW7c9Ox0CKSKG8iGegg/videos" bgColor="#ac0f1b" color="currentColor" target="_blank" />
         </li>
     </ul>
   )
