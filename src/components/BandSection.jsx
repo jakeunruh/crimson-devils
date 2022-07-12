8798e052c80775ffd9583421c1956333beac641a
jake unruh <jake.unruh@gmail.com>
2022-07-12 14:23:16 -0500

    fh-epk layout addition

---
+++ src/components/BandSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function BandSection() {
+  return (
+    <>
+        <section className='main__band-image'>
+        
+        </section>
+        <section className="main__band-info">
+            <ul>
+                <li>
+                    <h3>Jake Unruh</h3>
+                    <p>Guitars - Vocals</p>
+                </li>
+                <li>
+                    <h3>Patric Pascucci</h3>
+                    <p>Drums</p>
+                </li>
+                <li>
+                    <h3>Curt Christenson</h3>
+                    <p>Bass</p>
+                </li>
+            </ul>
+        </section>
+    </>
+  )
+}
+
+export default BandSection
--- src/pages/EpkFH.jsx
+++ src/pages/EpkFH.jsx
@@ -1,6 +1,8 @@
+import BandSection from "../components/BandSection";
 import FHEmbed from "../components/FHEmbed";
 import HeaderSmall from "../components/HeaderSmall"
 import SocialLinkList from "../components/SocialLinkList";
+import FH from "../img/epk/fh-hi-res.png";
 
 function EpkFH() {
   return (
@@ -8,15 +10,39 @@ function EpkFH() {
       <HeaderSmall />
       <main className="main">
         <h1>Forever High - New Album from Crimson Devils</h1>
-        <section className="main__bio-releases">
-          <section className="bio">
-            <h2>Forever High is the upcoming release from Crimson Devils</h2>
-            <p>Recorded at the BBQ Shack in Austin, Tx with Jason Morales, Forever High is the second record for Crimson Devils.</p>
+        <section className="main__album-audio">
+          <section className="album">
+            <img src={FH} alt="Forever High Album Art" />
           </section>
-          <section className="releases">
+          <section className="audio">
             <h2>Listen Now</h2>
             <FHEmbed />
           </section>
+        </section>
+        <section className="main__description-tracklist">
+          <section class="description">
+            <h2>Forever High is the upcoming release from Crimson Devils</h2>
+            <p>Recorded at the BBQ Shack in Austin, Tx with Jason Morales, Forever High is the second record for Crimson Devils.</p>
+          </section>
+          <section class="tracklist">
+            <h2>Tracklist</h2>
+            <ol>
+              <li>Feels Like The Last Time</li>
+              <li>Forever High</li>
+              <li>Home Again</li>
+              <li>Not My Time</li>
+              <li>Used Up</li>
+              <li>Count Back From Ten</li>
+              <li>Feign Free</li>
+              <li>Pressure Vessels</li>
+              <li>Lost My Mind</li>
+              <li>We Walk Alone</li>
+              <li>King Of The World</li>
+            </ol>
+          </section>
+        </section>
+        <section className="main__band">
+          <BandSection />
         </section>
       </main>
       <footer className="footer">
