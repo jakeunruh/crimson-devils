1afe864e69b68c181417a6d572375109d25a405f
jake unruh <jake.unruh@gmail.com>
2022-07-27 16:08:04 -0500

    updated embed for forever high epk

--- src/components/EmbedFH.jsx
+++ src/components/EmbedFH.jsx
@@ -1,10 +1,10 @@
 function EmbedFH() {
   return (
     <section className='iframe iframe--fh'>
-        <iframe title="Forever High Full Length Album" width="100%" height="450" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1467033004%3Fsecret_token%3Ds-KZy9bZoOC3s&color=%23b81a0a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        <iframe title="Forever High Full Length Album" width="100%" height="450" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1474707343%3Fsecret_token%3Ds-e7Oey0y7gJz&color=%23b81a0a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
         
         <div>
-            <a href="https://soundcloud.com/crimsondevilsatx" title="Crimson Devils" rel="noreferrer" target="_blank">Crimson Devils</a> · <a href="https://soundcloud.com/crimsondevilsatx/sets/forever-high/s-KZy9bZoOC3s" title="Forever High" rel="noreferrer" target="_blank">Forever High</a>
+            <a href="https://soundcloud.com/crimsondevilsatx" title="Crimson Devils" rel="noreferrer" target="_blank">Crimson Devils</a> · <a href="https://soundcloud.com/crimsondevilsatx/sets/forever-high" title="Forever High" rel="noreferrer" target="_blank">Forever High</a>
         </div>
     </section>
   )
--- src/pages/EpkFH.jsx
+++ src/pages/EpkFH.jsx
@@ -23,7 +23,7 @@ function EpkFH() {
           <section className="description">
             <h2>Forever High is the upcoming release from Crimson Devils</h2>
             <p>As the second release from <strong>Crimson Devils</strong>, <strong>Forever High</strong> is a progression and elevation of all the things that the band does best. Driving energy, tight grooves and all the attitude pushed to the limit. <strong>Forever High</strong> hands out eleven tight, focused cuts that showcase the bands power, technical abilites and the ability to write a song with a hook. No filler here, just all the bands best work in one tasty place.</p>
-            <p>Recorded and mixed by Jason Morales at the BBQ Shack.<br />Mastered by Jack Butcher at Enormous Door Mastering</p>
+            <p>Recorded and mixed by Jason Morales at the BBQ Shack.</p>
           </section>
           <section className="tracklist">
             <h2>Tracklist</h2>
