4423f248731fe2f98ea5f9fb79ee35f09943735c
jake unruh <jake.unruh@gmail.com>
2022-07-27 16:38:36 -0500

    adding mastering credits on epkFh page

--- src/pages/EpkFH.jsx
+++ src/pages/EpkFH.jsx
@@ -24,6 +24,7 @@ function EpkFH() {
             <h2>Forever High is the upcoming release from Crimson Devils</h2>
             <p>As the second release from <strong>Crimson Devils</strong>, <strong>Forever High</strong> is a progression and elevation of all the things that the band does best. Driving energy, tight grooves and all the attitude pushed to the limit. <strong>Forever High</strong> hands out eleven tight, focused cuts that showcase the bands power, technical abilites and the ability to write a song with a hook. No filler here, just all the bands best work in one tasty place.</p>
             <p>Recorded and mixed by Jason Morales at the BBQ Shack.</p>
+            <p>Matered by Jerry Tubb at Terra Nova Mastering, Austin, Texas</p>
           </section>
           <section className="tracklist">
             <h2>Tracklist</h2>
