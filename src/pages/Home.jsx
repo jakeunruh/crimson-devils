4ec7a8b13d86a3d34d946265326a786eec1b8288
jake unruh <jake.unruh@gmail.com>
2022-08-08 20:12:42 -0500

    add show widget to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import VideoSJ from "../components/VideoSJ";
 import LinkList from "../components/LinkList";
 import SocialLinkList from "../components/SocialLinkList";
 import React from "react";
+import ShowWidget from "../components/ShowWidget";
 
 function Home() {
   return (
     <>
       <Header />
       <main className="main">
+        <ShowWidget />
         <section className="main__feature-video">
           <VideoSJ />
         </section>
